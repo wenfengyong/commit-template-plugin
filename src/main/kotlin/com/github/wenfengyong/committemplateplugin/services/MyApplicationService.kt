362b39231f22081f761277168dc38cb2e20125bb
GitHub Action <action@github.com>
2022-06-11 07:08:29 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/wenfengyong/committemplateplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.wenfengyong.committemplateplugin.services
+
+import com.github.wenfengyong.committemplateplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/wenfengyong/committemplateplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/wenfengyong/committemplateplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.wenfengyong.committemplateplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wenfengyong.committemplateplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
